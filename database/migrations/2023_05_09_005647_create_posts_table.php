c7019e8a44e53b9c8465be689fab9dd1e6dba4e5
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-05-15 16:21:20 +0200

    used softdeletes

--- database/migrations/2023_05_09_005647_create_posts_table.php
+++ database/migrations/2023_05_09_005647_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 50);
             $table->string('body');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
